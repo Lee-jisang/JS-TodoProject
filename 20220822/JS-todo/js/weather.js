4b68d3534780c5a11cee61f93f04710ab0dda5b4
Lee-jisang <dlwltkd2003@naver.com>
2022-08-22 02:15:03 +0900

    0822

---
+++ 20220822/JS-todo/js/weather.js
@@ -0,0 +1,23 @@
+const weather = document.querySelector("#weather span:first-child");
+const city = document.querySelector("#weather span:last-child");
+const API_KEY = "13d5db7306395c1e8747ce051938af0e";
+
+function onGeoOk(position) {
+  console.log(position);
+  const lat = position.coords.latitude;
+  const lon = position.coords.longitude;
+  const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`;
+  console.log(url);
+  fetch(url)
+    .then((response) => response.json())
+    .then((data) => {
+      console.log(data.name, data.weather[0].main);
+      weather.innerText = `${data.weather[0].main} / ${data.main.temp}`;
+      city.innerText = data.name;
+    });
+}
+function onGeoError() {
+  alert("Can't find you. No weather for you.");
+}
+//성공했을때 onGeoOk, 실패했을때, onGeoError
+navigator.geolocation.getCurrentPosition(onGeoOk, onGeoError);
